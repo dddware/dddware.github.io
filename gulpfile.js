cf837a14c1d59b5810c2507c251d66289840693e
neemzy <tom.panier@free.fr>
2014-02-01 03:07:44 +0100

    fix conflict

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
     tasks = require('gulp-load-tasks')(),
     fs = require('fs'),
+    md = require('markdown'),
     mde = require('markdown-extra');
 
 
 
-gulp.task('test', function () {
+var parseOnProjects = function (callback) {
     fs.readdir('projects', function (err, files) {
         var projects = [],
             nb = files.length;
@@ -13,9 +14,11 @@ gulp.task('test', function () {
         [].forEach.call(files, function (file, i) {
             fs.readFile('projects/' + file, { encoding: 'utf8' }, function (err, markdown) {
                 if (! err) {
-                    projects.push({
-                        content: mde.content(markdown),
-                        metadata: mde.metadata(markdown, function (data) {
+                    var project = {
+                            content: md.parse(mde.content(markdown))
+                        },
+
+                        metadata = mde.metadata(markdown, function (data) {
                             var split = data.split("\n"),
                                 i;
 
@@ -23,24 +26,37 @@ gulp.task('test', function () {
 
                             for (i in split) {
                                 var line = split[i].split(': ');
-                                data[line[0]] = line[1];
+                                data[line[0]] = eval(line[1]);
                             }
 
                             return data;
-                        })
-                    });
+                        });
+
+                    for (var key in metadata) {
+                        if (metadata.hasOwnProperty(key)) {
+                            project[key] = metadata[key];
+                        }
+                    }
+
+                    projects.push(project);
 
                     if (i == nb - 1) {
-                        console.log(projects);
+                        callback(projects);
                     }
                 }
             });
         });
     });
-});
+};
+
+
 
 gulp.task('default', function() {
     var locals = {};
+
+    parseOnProjects(function (projects) {
+        locals.projects = projects;
+    });
 
     gulp.src('./assets/layout/index.jade')
         .pipe(tasks.jade({
