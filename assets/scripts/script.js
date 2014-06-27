efb7c39c04b2b6d4343b5448f98b9d6bf50e8b5b
victor darras <victor.darras@gmail.com>
2014-06-27 22:07:55 +0200

    change design

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
-	console.log('Hello world');
-})
+	var nav = document.querySelectorAll('nav a');
+	var pages = document.querySelectorAll('section');
+
+	[].forEach.call(nav, function(el) {
+	  el.addEventListener('click', function() {
+	  	[].forEach.call(nav, function(el) {
+	  		el.classList.remove('active');
+	  	});
+	  	[].forEach.call(pages, function(el) {
+	  		el.classList.remove('active');
+	  	});
+
+			el.classList.add('active');
+			(document.querySelector(el.hash)).classList.add('active')
+	  })
+	});
+
+});
